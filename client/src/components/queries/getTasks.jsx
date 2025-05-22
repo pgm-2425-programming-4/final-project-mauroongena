d4528d96ed327bfb4509f5bac3f3626468ec2e1d
mauroongena <mauro.ongena@icloud.com>
2025-05-22 14:39:17 +0200

    fix: update authorization token in getTasks function

--- client/src/components/queries/getTasks.jsx
+++ client/src/components/queries/getTasks.jsx
@@ -14,7 +14,7 @@ export async function getTasks({ page = 1, pageSize = 20, project } = {}) {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
-      Authorization: `Bearer 3b16462ac859b9600de06b6d717a276d3bb7eae0046bd6e821ffad3f39f4ab0137961eacbb561ba992850b22a90b6f495d4735a8dbd56f35da9cbfd3ab18ea88b0c06ec0b8ea75ef8acf957fd9135c225aec5ee974ce3692614aeb01d8ea64431e2b1b559e492c2e9a93a76dd9cf1c86c4c8b86bb9523242d11dfb580b1bbdd0`,
+      Authorization: `Bearer 7a8106939caf39bf75855e168ae602686ebeabf89bf79405398ccd032dae01ffda6f21ff8a82d6c574e454cb1fee69db0bfed46557dba1241791af2d4bbe25d0af59060ff57f35f51e2553518f4640900348dd12abf6d6c3ce676ff937f73417b7d52a2f153b4997799bb3d2bad18d12c4d91540f904d88068ed1f58f79cdb66`,
     },
   });
 
