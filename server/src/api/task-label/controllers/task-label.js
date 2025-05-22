c0f080df1d3dedb3b9e509d90d3daa4c9e9fa248
mauroongena <mauro.ongena@icloud.com>
2025-05-22 14:38:17 +0200

    Added server to dev branch

---
+++ server/src/api/task-label/controllers/task-label.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * task-label controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::task-label.task-label');
