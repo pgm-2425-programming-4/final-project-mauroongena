e47c82f44e393ea7e14bd89d96c5d02188f9644a
mauroongena <mauro.ongena@icloud.com>
2025-05-22 14:00:21 +0200

    Removed server in main branch for bug fix

--- server/src/api/task-label/routes/task-label.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * task-label router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::task-label.task-label');
--- server/src/api/task-label/services/task-label.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * task-label service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::task-label.task-label');
