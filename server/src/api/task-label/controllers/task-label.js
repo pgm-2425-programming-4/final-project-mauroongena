0f0347fabc45e200033d629a0a1894d2c9d84564
mauroongena <mauro.ongena@icloud.com>
2025-05-22 14:05:29 +0200

    Removed server for bug fixes

--- server/src/api/task-label/controllers/task-label.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * task-label controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::task-label.task-label');
