6aa0b3c244f252b6d22672959c2bf4ae4b4e230a
mauroongena <mauro.ongena@icloud.com>
2025-05-28 11:12:49 +0200

    feat: add link to backlog in HomePage and remove unused import in __root route

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,8 +1,10 @@
+import { Link } from '@tanstack/react-router';
 
 function HomePage() {
   return (
     <div>
       <h1 className="header__title">Welkom</h1>
+      <Link to="/backlog">Go to backlog</Link>
     </div>
   );
 }
